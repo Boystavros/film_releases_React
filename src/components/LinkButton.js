abbb1081512055dc3e49eff363226f4c83211ad2
Sacha Ponniah <sponniah812@gmail.com>
2022-01-04 17:44:10 +0000

    Complete.

---
+++ src/components/LinkButton.js
@@ -0,0 +1,7 @@
+const LinkButton = () => {
+    return (
+        <a href="https://www.imdb.com/calendar/?region=gb"><button>View more upcoming releases >> </button></a>
+    )
+}
+
+export default LinkButton;
---
+++ src/components/TitleBox.js
@@ -0,0 +1,10 @@
+const TitleBox = () => {
+    return (
+        <>
+            <h1>Upcoming Film Releases for UK</h1>
+            <hr/>
+        </>
+    )
+}
+
+export default TitleBox;
